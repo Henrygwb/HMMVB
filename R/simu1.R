14720207336c5db4d4744d2df760d729b005ead6
Henrygwb <wzg13@psu.edu>
2017-10-16 18:10:33 -0400

    Upload R files of data sets

---
+++ R/simu1.R
@@ -0,0 +1,8 @@
+#' @title Simulation data set 1
+#' @description Simulation data set with true labels
+#' @name simu1
+#' @docType data
+#' @usage simu1
+#' @format A matrix of data and A vector of labels
+#' @keywords dataset
+NULL
---
+++ R/simu2.R
@@ -0,0 +1,8 @@
+#' @title Simulation data set 2
+#' @description Simulation data set with true labels
+#' @name simu2
+#' @docType data
+#' @usage simu2
+#' @format A matrix of data and A vector of labels
+#' @keywords dataset
+NULL
